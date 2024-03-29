6767ede57201219093b28e75ef9e812ec5afb3c3
kaushik <prajapatikaushik6392@gmail.com>
2024-03-30 01:40:03 +0530

    Third  commit

--- 01_basics/07_date-time.js
+++ 01_basics/07_date-time.js
@@ -7,13 +7,13 @@ let myDate = new Date()
 
 console.log(myDate.toDateString());
 console.log(myDate.toISOString());
-// console.log(myDate.toJSON());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toLocaleString());
-// console.log(myDate.toLocaleTimeString());
-// console.log(myDate.toString());
-// console.log(myDate.toTimeString());
-// console.log(myDate.toUTCString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
 
 
 let myCreatedDate = new Date(2024, 0, 20)
